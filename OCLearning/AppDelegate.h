4eabc029e684f8f7619d323640d6d920a9522563
antdlx <antdlx@vip.qq.com>
2016-08-10 09:41:14 +0800

    Initial Commit

---
+++ OCLearning/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCLearning
+//
+//  Created by antdlxding on 8/10/16.
+//  Copyright © 2016 antdlxding. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OCLearning/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OCLearning
+//
+//  Created by antdlxding on 8/10/16.
+//  Copyright © 2016 antdlxding. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
